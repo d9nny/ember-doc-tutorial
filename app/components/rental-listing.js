555a90c6ae001c1f68bacddde89131e351622b05
danwheeler <da-wheeler@hotmail.co.uk>
2016-05-26 20:33:59 +0100

    ember doc tutorial complete

---
+++ app/components/rental-listing.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+	isImageShowing: false,
+
+	actions: {
+
+		imageShow() {
+			this.set('isImageShowing', true);
+		},
+
+		imageHide() {
+			this.set('isImageShowing', false);
+		}
+	}
+});
